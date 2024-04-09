09c0aff3ca830bd01fe857530e530e39d3295317
Mikhail Terekhov <termim@gmail.com>
2024-04-09 00:13:15 -0400

    Update example.

--- pyqtgraph/examples/BarGraphItem.py
+++ pyqtgraph/examples/BarGraphItem.py
@@ -10,44 +10,51 @@ from pyqtgraph.Qt import QtCore
 win = pg.plot()
 win.setWindowTitle('pyqtgraph example: BarGraphItem')
 
-x = np.arange(10)
+N=15
+max = 3*np.pi
+x=np.arange(0, max, max/N)
+d = max/N/3
+w = d
 y1 = np.sin(x)
-y2 = 1.1 * np.sin(x+1)
-y3 = 1.2 * np.sin(x+2)
-colors = 'rgb'
-colors = range(16)
-cm = pg.colormap.get('magma').getLookupTable(nPts=32, mode=pg.ColorMap.QCOLOR)
+y2 = np.sin(x+d)
+y3 = np.sin(x+2*d)
 
-bg1 = pg.BarGraphItem(x=x, height=y1, width=0.3, brush=colors[0])
-bg2 = pg.BarGraphItem(x=x+0.33, height=y2, width=0.3, brush=colors[1])
-bg3 = pg.BarGraphItem(x=x+0.66, height=y3, width=0.3, brush=colors[2])
+colors = range(16)
+cm = pg.colormap.get('plasma').getLookupTable(nPts=len(x)*2, mode=pg.ColorMap.QCOLOR)
+bc = list([ pg.mkBrush(cm[i]) for i in range(len(cm)) ])
+
+bg1 = pg.BarGraphItem(x=x, height=y1, width=w, brush=colors[0])
+bg2 = pg.BarGraphItem(x=x+d, height=y2, width=w, brush=colors[1])
+bg3 = pg.BarGraphItem(x=x+2*d, height=y3, width=w, brush=colors[2])
 
 win.addItem(bg1)
 win.addItem(bg2)
 win.addItem(bg3)
 
 i = 0
-def update():
+def update_pen():
     global i, bg1, bg2, bg3
     i += 1
     for j, bg in enumerate((bg1, bg2, bg3)):
-        bg.setPen(colors[i % len(colors)])
-        #if i % 10: return
-        #bg.setBrush(colors[(i + j) % len(colors)])
-ii = 0
-def update2():
-    global i, ii, bg1, bg2, bg3
-    if i % 10: return
-    ii += 1
-    br = list([pg.mkBrush(cm[(k+ii)%len(x)]) for k in range(len(x))])
-    for j, bg in enumerate((bg1, bg2, bg3)):
-        bg.setBrush(br)#cm[ii % len(cm)])
-        #bg.setBrush(colors[(ii + j) % len(colors)])
+        bg.setPen(colors[i % len(colors)], width=i%6)
 
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.timeout.connect(update2)
-timer.start(300)
+timer1 = QtCore.QTimer()
+timer1.timeout.connect(update_pen)
+timer1.start(500)
+
+ii = 0
+def update_brush():
+    global ii, bg1, bg2, bg3
+    ii += 1
+    ic0 = ii % len(bc)
+    for j, bg in enumerate((bg1, bg2, bg3)):
+        ic0 = (ii+j) % len(bc)
+        br = list([bc[(ic0+ic)%len(bc)] for ic in range(len(x))])
+        bg.setBrush(br)
+
+timer2 = QtCore.QTimer()
+timer2.timeout.connect(update_brush)
+timer2.start(500)
 
 # Final example shows how to handle mouse clicks:
 class BarGraph(pg.BarGraphItem):
@@ -55,7 +62,7 @@ class BarGraph(pg.BarGraphItem):
         print("clicked")
 
 
-bg = BarGraph(x=x, y=y1*0.3+2, height=0.4+y1*0.2, width=0.8)
+bg = BarGraph(x=x, y=y1*w+2, height=0.4+y1*0.2, width=w*2.4)
 win.addItem(bg)
 
 if __name__ == '__main__':
